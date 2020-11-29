5e81532c36fe7c2b23a372dc17eb340aae3e86e4
ralles <ralles.liu@gmail.com>
2020-11-29 16:18:19 -0500

    creating user connections

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -12,18 +12,4 @@ class Enrollment < ApplicationRecord
         class_name: :Course
     })
     
-        
-    # belongs_to()
-    # has_many(:users, {
-    #     primary_key: :id,
-    #     foreign_key: :student_id,
-    #     class_name: :User
-    # })
-
-    # has_many(:courses, {
-    #     primary_key: :id,
-    #     foreign_key: :course_id,
-    #     class_name: :Course
-    # })
-
 end
