ba44b88f72ec9bfee6a0f1bf936c31df7c46288b
ralles <ralles.liu@gmail.com>
2020-11-29 02:37:54 -0500

    creates associations with enrollment

--- skeleton/app/models/enrollment.rb
+++ skeleton/app/models/enrollment.rb
@@ -1,2 +1,28 @@
 class Enrollment < ApplicationRecord
+
+    belongs_to(:user, {
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :User
+    })
+
+    belongs_to(:course, {
+        primary_key: :id, 
+        foreign_key: :course_id, 
+        class_name: :Course
+    })
+    
+    # belongs_to()
+    # has_many(:users, {
+    #     primary_key: :id,
+    #     foreign_key: :student_id,
+    #     class_name: :User
+    # })
+
+    # has_many(:courses, {
+    #     primary_key: :id,
+    #     foreign_key: :course_id,
+    #     class_name: :Course
+    # })
+
 end
