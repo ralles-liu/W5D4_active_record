ba79d2a9f775dc9b206450142787603a2fa53fbc
ralles <ralles.liu@gmail.com>
2020-11-30 01:34:05 -0500

    creating the random short urls

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,6 +1,13 @@
+require 'securerandom'
 class ShortenedUrl < ApplicationRecord
     validates :short_url, presence: true
     validates :short_url, uniqueness: true
+
+    def random_code
+        SecureRandom.urlsafe_base64
+    end
+
+
 
 
 end
